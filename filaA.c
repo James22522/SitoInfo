688f1a2e8595779731ee093f6d6886c695c0fa0c
James22522 <jack220503@gmail.com>
2025-05-14 20:48:13 +0200

    Update filaA.c

--- filaA.c
+++ filaA.c
@@ -3,26 +3,25 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int main()
-{
+int main(){
     int f1, f2;
     
     f1 = fork();
     
     if(f1 == 0){
-        FILE *titolo = fopen("titolo.txt","w");
+        FILE *titolo = fopen("titolo.txt", "w");
         if(!titolo){
             printf("Errore apertura file");
             exit(1);
         }
-        fprintf(titolo, "Il mistero della montagna\n");
+        
+        fputs("Il mistero della montagna", titolo);
         fclose(titolo);
         exit(0);
     }else if(f1 > 0){
         wait(&f1);
         
         f2 = fork();
-        
         if(f2 == 0){
             char buffer[1024];
             FILE *opera = fopen("opera.txt","w");
@@ -30,24 +29,21 @@ int main()
                 printf("Errore apertura file");
                 exit(1);
             }
-            printf("Inserisci contenuto");
-            fgets(buffer, sizeof(buffer),stdin);
-            fprintf(opera, buffer);
+            
+            fgets(buffer, sizeof(buffer), stdin);
+            fputs(buffer, opera);
             fclose(opera);
             exit(0);
         }
-    }else{
-        printf("Errore generazione processo figlio");
-        exit(1);
     }
     
     wait(&f2);
     
-    FILE *titolo = fopen("titolo.txt","r");
-    FILE *opera = fopen("opera.txt","r");
     FILE *finale = fopen("finale.txt","w");
-
-    if(!opera || !titolo || !finale){
+    FILE *opera = fopen("opera.txt","r");
+    FILE *titolo = fopen("titolo.txt", "r");
+    
+    if(!finale || !opera || !titolo){
         printf("Errore apertura file");
         exit(1);
     }
@@ -64,6 +60,6 @@ int main()
     fclose(titolo);
     fclose(opera);
     fclose(finale);
-
+    
     return 0;
 }
